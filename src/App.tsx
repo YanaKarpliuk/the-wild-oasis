3a33fad0b2ae4af93e5214f0c75f393d2486ec6c
Yana Karpliuk <yana.karpliuk@lembergsolutions.com>
2026-03-19 21:19:18 +0200

    Set up react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import AppLayout from './ui/AppLayout';
 import Dashboard from './pages/Dashboard';
 import PageNotFound from './pages/PageNotFound';
@@ -9,6 +10,16 @@ import Settings from './pages/Settings';
 import Account from './pages/Account';
 import Login from './pages/Login';
 import GlobalStyles from './styles/GlobalStyles';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // Time for storing cached data (1 min).
+      staleTime: 60 * 1000
+    }
+  }
+});
 
 const router = createBrowserRouter([
   {
@@ -53,9 +64,10 @@ const router = createBrowserRouter([
 
 export default function App() {
   return (
-      <>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false}/>
         <GlobalStyles/>
         <RouterProvider router={router}/>
-      </>
-  )
+      </QueryClientProvider>
+  );
 }
